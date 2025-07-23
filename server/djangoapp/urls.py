02be28da63874a6cdb1c0906b9e7154453afeecf
Manin CHIEV <chievmanin@gmail.com>
2025-07-23 20:46:03 +0700

    Add review functionality with post_review method, enhance dealership retrieval by state, and implement dealer reviews and details endpoints

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,9 +18,13 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
-    
+    path('add_review', views.add_review, name='add_review'),
+
     # path for get dealers
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
 
     # path for get cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
 import logging
 import json
-from .restapis import get_request
+from .restapis import get_request, analyze_review_sentiments, post_review
 from .models import CarMake, CarModel
 from .populate import initiate
 
@@ -37,15 +37,13 @@ def login_user(request):
     return JsonResponse(data)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
-def get_dealerships(request):
-    if request.method == "GET":
-        try:
-            # Get dealers from the API
-            dealerships = get_request('dealerships')
-            return JsonResponse({"status": "success", "dealers": dealerships})
-        except Exception as e:
-            return JsonResponse({"status": "error", "message": str(e)})
-    return JsonResponse({"status": "error", "message": "Method not allowed"})
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `logout_request` view to handle sign out request
 @csrf_exempt
@@ -87,16 +85,40 @@ def registration(request):
         return JsonResponse(data)
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if(dealer_id):
+        endpoint = "/fetchDealer/"+str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status":200,"dealer":dealership})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+@csrf_exempt
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
